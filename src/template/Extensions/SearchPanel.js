a2e0903eccc7a230de74deae58fe1445a0be42d7
Chuong Ho <chuongpqvn@gmail.com>
2024-12-17 17:20:36 +0800

    update search data

--- src/template/Extensions/SearchPanel.js
+++ src/template/Extensions/SearchPanel.js
@@ -5,112 +5,113 @@ let guid = null;
 
 function SearchPanel(viewer, container, id, title, options) {
     this.viewer = viewer;
-    // get viewer name
+    console.log("SearchPanel: ", viewer);
     Autodesk.Viewing.UI.DockingPanel.call(this, container, id, title, options);
-    // the style of the docking panel
-    // use this built-in style to support Themes on Viewer 4+
+
+    // Panel container styling
     this.container.classList.add('docking-panel-container-solid-color-a');
     this.container.style.top = "10px";
     this.container.style.left = "10px";
     this.container.style.width = "auto";
     this.container.style.height = "auto";
     this.container.style.resize = "auto";
-    // this is where we should place the content of our panel
+
+    // Main content container
     var div = document.createElement('div');
-    // set style to docking panel allway in bottom right of the screen
     div.style.margin = '10px';
-    div.style.marginBottom = '10px';
     div.style.width = 'auto';
     div.style.height = 'auto';
 
+    // Add custom controls to the first row
+    var controlsDiv = document.createElement('div');
+    controlsDiv.id = "controls";
+    controlsDiv.style.position = "fixed";
+    controlsDiv.style.top = "10px";
+    controlsDiv.style.left = "10px";
+    controlsDiv.style.zIndex = "5";
+    controlsDiv.style.backgroundColor = "white";
+    controlsDiv.style.padding = "10px";
+    controlsDiv.style.borderRadius = "5px";
+    controlsDiv.style.boxShadow = "0 2px 2px rgba(0, 0, 0, 0.2)";
+
+    // Input field for Object ID
+    var objectIdInput = document.createElement('input');
+    objectIdInput.id = "objectId";
+    objectIdInput.type = "text";
+    objectIdInput.placeholder = "(Object Id/Search..)";
+    objectIdInput.style.margin = "5px 0";
+    objectIdInput.style.padding = "5px";
+    objectIdInput.style.width = "150px";
+    //add event enter
+    objectIdInput.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            searchItems(viewer);
+        }
+    });
+
+    // Buttons
+    function createButton(id, text, onClickHandler) {
+        var button = document.createElement('button');
+        button.id = id;
+        button.innerText = text;
+        button.style.margin = "5px";
+        button.style.padding = "5px";
+        button.style.fontSize = "12px";
+        button.style.cursor = "pointer";
+        button.addEventListener('click', onClickHandler);
+        return button;
+    }
+
+    var zoomInButton = createButton('zoomInBtn', 'Zoom In', function () {
+        zoomIn();
+    });
+    var zoomOutButton = createButton('zoomOutBtn', 'Zoom Out', function () {
+        zoomOut();
+    });
+    var isolateButton = createButton('isolateBtn', 'Isolate', function () {
+        isolateObject();
+    });
+    var searchButton = createButton('searchBtn', 'Search', function () {
+        searchItems(viewer);
+    });
 
 
-    // add a checkbox for option search by element id
-    var checkbox = document.createElement('input');
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.setAttribute("id", "isDbId");
-    checkbox.setAttribute("name", "isDbId");
-    checkbox.setAttribute("value", "dbId");
-    checkbox.setAttribute("class", "isDbId");
-    checkbox.style.margin = '10px';
-    checkbox.style.width = 'auto';
-    checkbox.style.height = 'auto';
-    checkbox.style.display = 'absolute';
-    div.appendChild(checkbox);
-    var label = document.createElement('label');
-    label.setAttribute("for", "radio");
-    label.innerText = "Search by DbId";
-    label.style.margin = '10px';
-    label.style.width = 'auto';
-    label.style.height = 'auto';
-    label.style.display = 'absolute';
-    div.appendChild(label);
+    // Add controls to the div
+    controlsDiv.appendChild(objectIdInput);
+    controlsDiv.appendChild(zoomInButton);
+    controlsDiv.appendChild(zoomOutButton);
+    controlsDiv.appendChild(isolateButton);
+    controlsDiv.appendChild(searchButton);
+    // Append the control panel to the main content
+    div.appendChild(controlsDiv);
 
-    // create a search box and button to execute search
-    var field = document.createElement('input');
-    field.setAttribute("type", "text");
-    field.setAttribute("id", "search-field");
-    field.setAttribute("placeholder", "Search...");
-    field.setAttribute("name", "search-field");
-    field.setAttribute("value", "");
-    field.setAttribute("class", "search-field");
-    field.style.marginRight = '10px';
-    div.appendChild(field);
-    var searchButton = document.createElement('button');
-    searchButton.setAttribute("type", "button");
-    searchButton.setAttribute("id", "btn-search");
-    searchButton.setAttribute("class", "btn-search");
-    searchButton.innerText = "Search";
-    searchButton.style.justifyContent = 'center';
-    searchButton.style.alignItems = 'center';
-    // set text color to white
-    searchButton.style.color = 'white';
-    // set tooltip
-    searchButton.setAttribute("title", "Search Properties");
-
-    div.appendChild(searchButton);
-
-
-
-    // add search results to the panel
+    // Search results container
     var results = document.createElement('div');
     results.setAttribute("id", "search-results");
     results.setAttribute("class", "search-results");
     div.appendChild(results);
-    // add event listener to the search button
-    searchButton.addEventListener('click', function () {
-        searchItems(viewer);
-    });
-    // add event enter key to the search field
-    field.addEventListener('keyup', function (event) {
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            searchButton.click();
-        }
-    });
-    this.container.appendChild(div);
-    // and may also append child elements...
 
+    // Append to the docking panel
+    this.container.appendChild(div);
 }
 
 SearchPanel.prototype = Object.create(Autodesk.Viewing.UI.DockingPanel.prototype);
 SearchPanel.prototype.constructor = SearchPanel;
 
-
 // search model properties by filters
 function searchItems(viewer) {
 
-    let searchValue = document.getElementById("search-field").value;
+    let searchValue = document.getElementById("objectId").value;
     console.log("searchValue: ", searchValue);
     if (searchValue === "") {
         viewer.fitToView();
         return;
     }
-    // if user input dbId
-    let flag = document.getElementById("isDbId").checked;
-    if (flag === true && searchValue.match(/^[0-9]+$/) != null) {
+    // if searchValue is number meaning flag is true
+    if (searchValue.match(/^[0-9]+$/) != null) {
         // get select by object id
-        let dbId = parseInt(document.getElementById("search-field").value);
+        let dbId = parseInt(document.getElementById("objectId").value);
         // find object id in model
         if (viewer.model.getData().instanceTree.nodeAccess.dbIdToIndex[dbId] != null) {
             // zoom to that object and isolate
@@ -122,20 +123,22 @@ function searchItems(viewer) {
     }
     // if user input match 45 characters include string and number, allow console information of object properties
     // example : 5bb069ca-e4fe-4e63-be31-f8ac44e80d30-0004718e
-    if (document.getElementById("search-field").value.match(/^[a-z0-9]{8}(-[a-z0-9]{4}){3}-[a-z0-9]{12}-[a-z0-9]{8}$/) != null) {
+    if (document.getElementById("objectId").value.match(/^[a-z0-9]{8}(-[a-z0-9]{4}){3}-[a-z0-9]{12}-[a-z0-9]{8}$/) != null) {
         // get select by guid
-        guid = document.getElementById("search-field").value;
+        guid = document.getElementById("objectId").value;
         // find external id in model : https://aps.autodesk.com/blog/get-dbid-externalid
         console.log("externalId: ", guid);
-        viewer.model.getExternalIdMapping(function (mapping) {  
+        viewer.model.getExternalIdMapping(function (mapping) {
             mappingAndIsolate(mapping, viewer);
         });
         return;
     }
     viewer.search(
-        document.getElementById("search-field").value,
+        document.getElementById("objectId").value,
         function (dbIDs) {
             viewer.isolate(dbIDs);
+            // select objects
+            viewer.select(dbIDs);
             viewer.fitToView(dbIDs);
             // set color to object
             //viewer.setThemingColor(dbIDs, new THREE.Vector4(0, 1, 0, 1));
@@ -158,15 +161,73 @@ function zoomAndIsoObject(viewer, dbId) {
     viewer.isolate(dbId);
     viewer.fitToView(dbId);
 }
+
 function selectObject(viewer, dbId) {
     console.log('Select Object: ', dbId);
     viewer.select(dbId);
     viewer.fitToView(dbId);
 }
 
+// Zoom In function
+function zoomIn() {
+    const objects = getSelectedOrInputObject(this.viewer);
+    if (objects && objects.length > 0) {
+        this.viewer.fitToView(objects);
+        // select objects
+        this.viewer.select(objects);
+        console.log(`Zoomed to objects: ${objects}`);
+    } else {
+        console.log('No object selected or input to zoom in.');
+    }
+}
+
+// Zoom Out function
+function zoomOut() {
+    const objects = getSelectedOrInputObject(this.viewer);
+    if (objects && objects.length > 0) {
+        this.viewer.navigation.fitBounds(true);
+        this.viewer.fitToView();
+        console.log(`Zoomed out for objects: ${objects}`);
+    } else {
+        this.viewer.fitToView();
+        console.log('Zoomed out to the full model.');
+    }
+}
+
+// Isolate function
+function isolateObject() {
+    const objects = getSelectedOrInputObject(this.viewer);
+    if (objects && objects.length > 0) {
+        this.viewer.isolate(objects); // Isolate specified objects
+        this.viewer.fitToView(objects); // Zoom into isolated objects
+        console.log(`Isolated objects: ${objects}`);
+    } else {
+        console.log('No object selected or input to isolate.');
+    }
+}
+
+// Helper function to get object from input or selection
+function getSelectedOrInputObject(viewer) {
+    if (viewer.getSelection().length > 0) {
+        return viewer.getSelection(); // Return selected objects
+    }
+    const inputId = document.getElementById('objectId').value;
+    // if contains, parse to list of object IDs
+    if (inputId.includes(',')) {
+        return inputId.split(',').map(id => parseInt(id));
+    }
+    if (inputId) {
+        const parsedId = parseInt(inputId);
+        if (!isNaN(parsedId)) {
+            return [parsedId]; // Return array of object ID
+        }
+        console.error('Invalid Object ID input.');
+    }
+    return; // Fallback to current selection
+}
 
 // allow search by element id in revit
-function searchByElementId(viewer,elementId){
+function searchByElementId(viewer, elementId) {
     // get all name of object
     let allName = [];
     // get all object id
@@ -174,12 +235,12 @@ function searchByElementId(viewer,elementId){
     let objects = viewer.model.getData().instanceTree.nodeAccess.dbIdToIndex;
     for (var key in objects) {
         if (objects.hasOwnProperty(key)) {
-            let name =viewer.model.getData().instanceTree.getNodeName(key);
-            if(name!==undefined && name.includes(elementId)){
+            let name = viewer.model.getData().instanceTree.getNodeName(key);
+            if (name !== undefined && name.includes(elementId)) {
                 allName.push(name);
                 allDbId.push(key);
             }
         }
     }
-   return allDbId;
+    return allDbId;
 }
